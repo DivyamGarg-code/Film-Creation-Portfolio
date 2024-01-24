dfe74a4516fdf20a51d62d8272536b115547a28e
Divyam Garg <dvm.garg26@gmail.com>
2024-01-24 23:54:07 +0530

    mail added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import mail from '../images/mail.svg'
 
 function Contact() {
   return (
-    <a className='contact_btn btn' href="mailto:">
+    <a className='contact_btn btn' href="mailto:productions.hinterland@gmail.com">
       <img src={mail} alt="error" />
       <div >Send Mail</div>
     </a>
