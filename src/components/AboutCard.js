cbdc9721e80e724aa43f6c898196b55f5b0f485f
Divyam Garg <dvm.garg26@gmail.com>
2024-05-28 15:56:01 +0530

    fixed bugs

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -5,9 +5,9 @@ function AboutCard() {
     useScrollingEffect();
     return (
         <div className='about_card_container' data-aos="fade-up">
-            <span className='h6'>The team at Cinematic Odyssey is truly dedicated to their craft, delivering exceptional films that engage and entertain audiences.</span>
-            <span className='h7'>Join our esteemed list of clients who trust us to bring their vision to life.</span>
-            <div className='about_items_container h8'>
+            <span className='text-[20px] md:h6'>The team at Cinematic Odyssey is truly dedicated to their craft, delivering exceptional films that engage and entertain audiences.</span>
+            <span className='text-[18px] md:h7'>Join our esteemed list of clients who trust us to bring their vision to life.</span>
+            <div className='about_items_container text-[18px] md:h8'>
                 <span>Films</span>
                 <span>Feature</span>
                 <span>Journey</span>
